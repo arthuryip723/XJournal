f0eca92ea5f40f51ee9a0cb248157ddcd0a94961
arthuryip723 <arthuryip723@gmail.com>
2013-10-30 17:00:32 -0400

    20131030

--- XJournal/journals/models.py
+++ XJournal/journals/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Journal(models.Model):
-    title = models.CharField()
-    text = models.CharField()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    user = models.ForeignKey(User)

4e1cdccbd27c23a363127ae9304671ef78bbe97d
arthuryip723 <arthuryip723@gmail.com>
2013-11-15 17:02:49 -0500

    20131115

--- XJournal/journals/models.py
+++ XJournal/journals/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class Journal(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     user = models.ForeignKey(User)
+    
+class JournalForm(ModelForm):
+    class Meta:
+        model = Journal
--- XJournal/journals/urls.py
+++ XJournal/journals/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from journals import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^new$', views.index, name='new')
 )
 

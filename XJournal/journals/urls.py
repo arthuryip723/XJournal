a8510d73636d2e5983a4be56c9c075f91fcf8198
arthuryip723 <arthuryip723@gmail.com>
2013-11-21 15:24:59 -0500

    20131121

--- XJournal/journals/urls.py
+++ XJournal/journals/urls.py
@@ -4,6 +4,6 @@ from journals import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^new$', views.index, name='new')
+    url(r'^new$', views.new, name='new')
 )
 
--- XJournal/journals/views.py
+++ XJournal/journals/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from journals.models import JournalForm
+from django.shortcuts import render_to_response
 
 def index(request):
 #     return HttpResponse("Hello, world. You're at the journal index.")
     return HttpResponse(request.user.username == 'su')
+
+def new(request):
+    if request.POST:
+        form = JournalForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = JournalForm()
+    
+    return render_to_response('journals/new.html', {'form': form})

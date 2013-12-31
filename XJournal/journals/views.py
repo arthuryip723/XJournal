fb5521e2b657bf90b50ad4ccfcc23b2e77a502de
arthuryip723 <arthuryip723@gmail.com>
2013-12-31 18:22:43 -0500

    20131231

--- XJournal/journals/views.py
+++ XJournal/journals/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, redirect, get_object_or_404, \
     render_to_response
 from django.utils import timezone
 from django.views import generic
+from django.core.servers.basehttp import FileWrapper
+from django.core.files.temp import NamedTemporaryFile
 
 from journals.models import JournalForm, Journal
-
+import os, StringIO
 
 # return journal list for the logged-in user
 # make a public index, and then make a private index
@@ -54,13 +56,45 @@ class DetailView(generic.DetailView):
 
 def edit(request, journal_id):
 #     record = Journal.objects.get(pk=journal_id)
-    journal = get_object_or_404(Journal, pk=journal_id)
-    form = JournalForm(instance=journal)
+    if request.POST:
+        return redirect('/journals')
+        #redirect to index?
+    else:
+        journal = get_object_or_404(Journal, pk=journal_id)
+        form = JournalForm(instance=journal)
     return render(request, 'journals/edit.html', {'form': form})
+
+def delete(request, journal_id):
+    journal = get_object_or_404(Journal, pk=journal_id)
+    pass
 
 # view a journal specified by the id
 
 # implement the crud operations
 
 # get a pdf from journals
+# def getPDF(request, journal_id):
+def getPDF(request,):
+#     journal = get_object_or_404(Journal, pk=journal_id)
+    # Print the pdf
+#     f = NamedTemporaryFile(suffix='.txt')
+    pdf = open("test.pdf", "r+")
+#     f.write(pdf.read())
+#     f.flush()
+#     f.write("hello world!")
+#     f.flush()
+#     print pdf.read()
+#     print f.read()
+#     f.flush()
+#     buffer = StringIO.StringIO()
+    response = HttpResponse(FileWrapper(pdf), content_type='application/pdf')
+#     response = HttpResponse(FileWrapper(f), content_type='application/txt')
+    response['Content-Disposition'] = 'attachment; filename=journal.txt'
+#     os.remove("foo.txt")
+    return response
+#     fo = open("foo.txt", "wb")
+#     fo.write("Python is a great language.\nYeah it's great!!\n");
+#     return HttpResponse("Successful")
+
+
 # write a filter for the selection of journals, i.e. from date to date, type, or even using checkbox

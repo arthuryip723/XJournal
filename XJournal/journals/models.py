d24c402c47afd3b7fcddf3e7177a4eac026f44a6
arthuryip723 <arthuryip723@gmail.com>
2014-01-10 18:46:50 -0500

    20140110

--- XJournal/journals/models.py
+++ XJournal/journals/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class Journal(models.Model):
     title = models.CharField(max_length=255)
